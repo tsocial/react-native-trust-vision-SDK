ec298e01010ef0b8a0af851becc3883c17841d61
TrangDaoTS <trang@trustingsocial.com>
2020-03-11 11:23:42 +0700

    update sdk add framework module

--- android/src/main/java/com/reactlibrary/RNTrustVisionRnsdkFrameworkModule.java
+++ android/src/main/java/com/reactlibrary/RNTrustVisionRnsdkFrameworkModule.java
@@ -3,29 +3,32 @@ package com.reactlibrary;
 
 import android.app.Activity;
 import android.text.TextUtils;
+import android.util.Base64;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.WritableNativeArray;
-import com.google.android.gms.common.util.CollectionUtils;
-import com.google.gson.JsonArray;
-import com.trustingsocial.apisdk.utils.GsonUtils;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.trustingsocial.apisdk.TVApi;
+import com.trustingsocial.apisdk.data.TVApiError;
+import com.trustingsocial.apisdk.data.TVCallback;
+import com.trustingsocial.tvsdk.TVCapturingCallBack;
 import com.trustingsocial.tvsdk.TVDetectionError;
+import com.trustingsocial.tvsdk.TVDetectionResult;
+import com.trustingsocial.tvsdk.TVIDConfiguration;
+import com.trustingsocial.tvsdk.TVSDKCallback;
 import com.trustingsocial.tvsdk.TVSDKConfiguration;
-import com.trustingsocial.tvsdk.TVSDKUtil;
+import com.trustingsocial.tvsdk.TVSelfieConfiguration;
+import com.trustingsocial.tvsdk.TVTransactionData;
 import com.trustingsocial.tvsdk.internal.TVCardType;
 import com.trustingsocial.tvsdk.internal.TrustVisionSDK;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.Collections;
 import java.util.List;
 
 public class RNTrustVisionRnsdkFrameworkModule extends ReactContextBaseJavaModule {
@@ -95,12 +98,154 @@ public class RNTrustVisionRnsdkFrameworkModule extends ReactContextBaseJavaModul
     }
 
     @ReactMethod
-    public void startFlowWithConfig(ReadableMap config, Promise promise) {
+    public void startFlowWithConfig(ReadableMap config, final Promise promise) {
         try {
             TVSDKConfiguration configuration = RNTrustVisionUtils.convertConfigFromMap(config);
-            TrustVisionSDK.startTrustVisionSDK(getCurrentActivity(), configuration);
+            TrustVisionSDK.startTrustVisionSDK(getCurrentActivity(), configuration, new TVCapturingCallBack() {
+                @Override
+                public void onError(TVDetectionError tvDetectionError) {
+                    promise.reject(tvDetectionError.getDetailErrorCode(), RNTrustVisionUtils.convertErrorString(tvDetectionError));
+                }
+
+                @Override
+                public void onSuccess(TVDetectionResult tvDetectionResult) {
+                    //convert to map
+                    promise.resolve(RNTrustVisionUtils.convertResult(tvDetectionResult));
+                }
+            });
         } catch (Exception ex) {
             promise.reject(ex);
         }
     }
+
+    @ReactMethod
+    public void startIDCapturing(ReadableMap config, final Promise promise) {
+        try {
+            TVIDConfiguration configuration = RNTrustVisionUtils.convertIdConfigFromMap(config);
+            TrustVisionSDK.startIDCapturing(getCurrentActivity(), configuration, new TVCapturingCallBack() {
+                @Override
+                public void onError(TVDetectionError tvDetectionError) {
+                    promise.reject(tvDetectionError.getDetailErrorCode(), RNTrustVisionUtils.convertErrorString(tvDetectionError));
+                }
+
+                @Override
+                public void onSuccess(TVDetectionResult tvDetectionResult) {
+                    promise.resolve(RNTrustVisionUtils.convertResult(tvDetectionResult));
+                }
+            });
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+    }
+
+    @ReactMethod
+    public void startSelfieCapturing(ReadableMap config, final Promise promise) {
+        try {
+            TVSelfieConfiguration configuration = RNTrustVisionUtils.convertSelfieConfigFromMap(config);
+            TrustVisionSDK.startSelfieCapturing(getCurrentActivity(), configuration, new TVCapturingCallBack() {
+                @Override
+                public void onError(TVDetectionError tvDetectionError) {
+                    promise.reject(tvDetectionError.getDetailErrorCode(), RNTrustVisionUtils.convertErrorString(tvDetectionError));
+                }
+
+                @Override
+                public void onSuccess(TVDetectionResult tvDetectionResult) {
+                    promise.resolve(RNTrustVisionUtils.convertResult(tvDetectionResult));
+                }
+            });
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+    }
+
+    @ReactMethod
+    public void faceMatching(String imageId1, String imageId2, final Promise promise) {
+        try {
+            TrustVisionSDK.faceMatching(imageId1, imageId2, new TVCapturingCallBack() {
+                @Override
+                public void onError(TVDetectionError tvDetectionError) {
+                    promise.reject(tvDetectionError.getDetailErrorCode(), RNTrustVisionUtils.convertErrorString(tvDetectionError));
+                }
+
+                @Override
+                public void onSuccess(TVDetectionResult tvDetectionResult) {
+                    promise.resolve(RNTrustVisionUtils.convertResult(tvDetectionResult));
+
+                }
+            });
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+    }
+
+    @ReactMethod
+    public void startTransaction(String referenceId, final Promise promise) {
+        try {
+            TrustVisionSDK.startTransaction(referenceId, new TVSDKCallback<TVTransactionData>() {
+                @Override
+                public void onSuccess(TVTransactionData data) {
+                    promise.resolve(data);
+
+                }
+
+                @Override
+                public void onError(TVDetectionError tvDetectionError) {
+                    promise.reject(tvDetectionError.getDetailErrorCode(), RNTrustVisionUtils.convertErrorString(tvDetectionError));
+                }
+            });
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+    }
+
+    @ReactMethod
+    public void endTransaction(final Promise promise) {
+        try {
+            TrustVisionSDK.endTransaction(getCurrentActivity(), new TVSDKCallback<String>() {
+                @Override
+                public void onSuccess(String transactionId) {
+                    WritableMap data = new WritableNativeMap();
+                    data.putString("transactionId", transactionId);
+                    promise.resolve(data);
+                }
+
+                @Override
+                public void onError(TVDetectionError tvDetectionError) {
+                    promise.reject(tvDetectionError.getDetailErrorCode(), RNTrustVisionUtils.convertErrorString(tvDetectionError));
+                }
+            });
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+    }
+
+    @ReactMethod
+    public void getLivenessOptions(Promise promise) {
+        try {
+            promise.resolve(TrustVisionSDK.getLivenessOptions());
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+    }
+
+    @ReactMethod
+    public void downloadImage(String imageId, final Promise promise) {
+        try {
+            TVApi.getInstance().downloadImage(imageId, new TVCallback<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    promise.resolve(Base64.encode(bytes, Base64.NO_WRAP));
+                }
+
+                @Override
+                public void onError(List<TVApiError> list) {
+                    TVDetectionError error = new TVDetectionError(list.get(0));
+                    promise.reject(error.getDetailErrorCode(), RNTrustVisionUtils.convertErrorString(error));
+                }
+            });
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+    }
+
 }
